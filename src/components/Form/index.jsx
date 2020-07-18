28d87d76b64f3cbe83c9872ebd25583ee538b121
Enes Öncan <enes.oncan@cimri.com>
2020-07-18 12:41:17 +0300

    First commit

---
+++ src/components/Form/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './styles.css';
+
+const Form = () => {
+    return (
+        <div className="form-wrapper">
+           <form>
+               <input type="text" placeholder="isim" />
+           </form>
+        </div>
+    )
+}
+
+export default Form;
+
